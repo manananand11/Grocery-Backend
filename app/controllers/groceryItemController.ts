480dda8e5952c6a59dbb046ee347648f2c455c68
MANAN <manananand11@gmail.com>
2024-04-11 23:09:10 +0530

    Create Grocery Item

--- app/controllers/groceryItemController.ts
+++ app/controllers/groceryItemController.ts
@@ -1,18 +1,24 @@
+import { NextFunction, Request, Response } from "express";
 import GroceryItemService from "../services/GroceryItemService";
 
 class GroceryItemController {
-  async createGroceryItem(req:any, res:any) {
-    const { name, price, quantity, description } = req.body;
+  async createGroceryItem(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name, price, quantity, description } = req.body;
+      const addedBy = parseInt(req.headers["userid"] as string, 10);
 
-    const groceryItemService = new GroceryItemService();
-    const groceryItem = await groceryItemService.createGroceryItem({
+      const groceryItemService = new GroceryItemService();
+      const groceryItem = await groceryItemService.createGroceryItem({
         name,
         price,
         quantity,
         description,
-        addedyBy:req.user.id,
-    });
-    res.status(201).json(groceryItem);
+        addedBy,
+      });
+      res.status(201).json(groceryItem);
+    } catch (error) {
+      next(error);
+    }
   }
   async getGroceryItemById() {}
   async getAllGroceryItems() {}
--- app/services/GroceryItemService.ts
+++ app/services/GroceryItemService.ts
@@ -1,14 +1,19 @@
 import GroceryItem from "../models/GroceryItem";
 
 class GroceryItemService{
-    async createGroceryItem({ name, price, quantity, description,addedyBy }) {
-    
+    async createGroceryItem({ name, price, quantity, description,addedBy }:{
+    name: string, 
+    price: number, 
+    quantity: number, 
+    description: string, 
+    addedBy: number, 
+    }) {
         const groceryItem = await GroceryItem.create({
             name,
             price,
             quantity,
             description,
-            addedyBy,
+            addedBy,
         });
         return groceryItem;
       }
